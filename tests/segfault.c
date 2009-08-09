405e5e5bb03aafd7d356cd1e16941fd939a2860a
Doug Rabson <dfr@rabson.org>
2009-08-09 09:18:34 +0100

    Add some test material.

---
+++ tests/segfault.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <unistd.h>
+
+long long factorial(long long n)
+{
+    if (n == 0) {
+	*(int*) 0 = 1;
+	return 1;
+    }
+    return n * factorial(n - 1);
+}
+
+int main(int argc, char** argv)
+{
+    printf("factorial(10)=%d\n", (int) factorial(10));
+}
